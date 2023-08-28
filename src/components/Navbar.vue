0b075c56aab12dc2b15b66ee58344bdf4f9296df
Kamron or Камрон <kamrush6429@gmail.com>
2023-08-28 15:46:36 +0500

    added about information

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,18 +5,28 @@ import { RouterLink } from "vue-router";
 <template>
   <div class="">
     <nav class="flex justify-between items-center h-[80px]">
-      <Router-Link to="/" class="text-3xl font-semibold text-[#1E0E62]">LOGO</Router-Link>
+      <Router-Link to="/" class="text-3xl font-semibold text-[#1E0E62]"
+        >LOGO</Router-Link
+      >
       <ul class="flex gap-5">
-        <li class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all">
+        <li
+          class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all"
+        >
           <Router-Link to="/"> Home </Router-Link>
         </li>
-        <li class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all">
+        <li
+          class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all"
+        >
           <Router-Link to="/about"> About </Router-Link>
         </li>
-        <li class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all">
+        <li
+          class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all"
+        >
           <Router-Link to="/projects"> Projects </Router-Link>
         </li>
-        <li class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all">
+        <li
+          class="text-[20px] font-medium text-gray-500 hover:text-[#1E0E62] transition-all"
+        >
           <Router-Link to="/contact"> Contact </Router-Link>
         </li>
       </ul>
