e002bfb0b06186b8d9af65af5956bf7256c146a2
Kamron or Камрон <kamrush6429@gmail.com>
2023-09-13 13:31:36 +0500

    fix nav active bug

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -17,7 +17,7 @@ const props = defineProps({
   <div
     class="text-[20px] text-[#666666] hover:text-[#1e0e62] font-semibold transition-all"
   >
-    <router-link :to="item.link">{{ item.title }}</router-link>
+    <router-link active-class="active" :to="item.link">{{ item.title }}</router-link>
   </div>
 </template>
 
