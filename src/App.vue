8c3a9c6e6047f7bc985597609b50c8a741f62294
Kamron or Камрон <kamrush6429@gmail.com>
2023-09-23 11:28:54 +0500

    fix

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const visible = ref(false);
 
 setTimeout(() => {
   visible.value = true;
-}, 0);
+}, 4000);
 </script>
 
 <template>
@@ -52,9 +52,11 @@ setTimeout(() => {
   background-color: transparent;
   padding: 50px;
 }
+
 .area {
   width: 100%;
 }
+
 .circles {
   position: absolute;
   top: 0;
@@ -64,6 +66,7 @@ setTimeout(() => {
   overflow: hidden;
   z-index: -100;
 }
+
 .circles li {
   position: absolute;
   display: block;
@@ -74,12 +77,14 @@ setTimeout(() => {
   animation: animate 25s linear infinite;
   bottom: -150px;
 }
+
 .circles li:nth-child(1) {
   left: 25%;
   width: 80px;
   height: 80px;
   animation-delay: 0s;
 }
+
 .circles li:nth-child(2) {
   left: 10%;
   width: 20px;
@@ -87,12 +92,14 @@ setTimeout(() => {
   animation-delay: 2s;
   animation-duration: 12s;
 }
+
 .circles li:nth-child(3) {
   left: 70%;
   width: 20px;
   height: 20px;
   animation-delay: 4s;
 }
+
 .circles li:nth-child(4) {
   left: 40%;
   width: 60px;
@@ -100,24 +107,28 @@ setTimeout(() => {
   animation-delay: 0s;
   animation-duration: 18s;
 }
+
 .circles li:nth-child(5) {
   left: 65%;
   width: 20px;
   height: 20px;
   animation-delay: 0s;
 }
+
 .circles li:nth-child(6) {
   left: 75%;
   width: 110px;
   height: 110px;
   animation-delay: 3s;
 }
+
 .circles li:nth-child(7) {
   left: 35%;
   width: 150px;
   height: 150px;
   animation-delay: 7s;
 }
+
 .circles li:nth-child(8) {
   left: 50%;
   width: 25px;
@@ -125,6 +136,7 @@ setTimeout(() => {
   animation-delay: 15s;
   animation-duration: 45s;
 }
+
 .circles li:nth-child(9) {
   left: 20%;
   width: 15px;
@@ -132,6 +144,7 @@ setTimeout(() => {
   animation-delay: 2s;
   animation-duration: 35s;
 }
+
 .circles li:nth-child(10) {
   left: 85%;
   width: 150px;
@@ -139,16 +152,17 @@ setTimeout(() => {
   animation-delay: 0s;
   animation-duration: 11s;
 }
+
 @keyframes animate {
   0% {
     transform: translateY(0) rotate(0deg);
     opacity: 1;
     border-radius: 0;
   }
+
   100% {
     transform: translateY(-1000px) rotate(720deg);
     opacity: 0;
     border-radius: 50%;
   }
-}
-</style>
+}</style>
